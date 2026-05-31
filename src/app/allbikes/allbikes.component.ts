62d287a0114a223b5a7bd470cb5d1753d4941820
Reem-Ashraff <115889280+Reem-Ashraff@users.noreply.github.com>
2026-06-01 01:31:31 +0300

    Update allbikes.component.ts

--- src/app/allbikes/allbikes.component.ts
+++ src/app/allbikes/allbikes.component.ts
@@ -17,7 +17,7 @@ export class AllbikesComponent {
     this.apiservice.all_bikes()
     .subscribe({next:(data:any)=>{
       for(let item of data[0]){
-        item.image="http://localhost:8080/bikes_rental_laravel/public/storage/"+item.image
+        item.image="https://laravelbikesrental-production.up.railway.app/storage/"+item.image
       }
       this.bikes = data[0];
       this.bikes_num = data[1];
